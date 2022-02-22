085f446b057e5c5db03ecc077e5cda3810496bbe
mohammed hossein mazandaranian <modos.m98@gmail.com>
2022-02-22 13:10:09 +0330

    Delete dist directory

--- dist/_nuxt/static/1645521600/blog/tags/html/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645521600",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Ftags\u002Fhtml",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ee7052d4"}}}}(null));
